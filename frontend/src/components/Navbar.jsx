e8d880fea994d2900feedbb5b22aef831e59775b
Vishwas7803 <vishwassengar0@gmail.com>
2025-06-24 16:50:55 +0530

    add login

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/Posts">Posts</Link></li>
+        <li><Link to="/Posts">Login</Link></li>
       </ul>
     </nav>
   );
